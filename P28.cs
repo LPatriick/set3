f875a76234b76faf9d6d40616e5e5497012cdecb
LPatriick <lenghelpatrick@gmail.com>
2025-01-26 15:36:41 +0200

    Create P28.cs

---
+++ P28.cs
@@ -0,0 +1,51 @@
+using System;
+
+class Program
+{
+    static int Partition(int[] arr, int low, int high)
+    {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++)
+        {
+            if (arr[j] < pivot)
+            {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp2 = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp2;
+        return i + 1;
+    }
+  
+    static void QuickSort(int[] arr, int low, int high)
+    {
+        if (low < high)
+        {
+            int pivotIndex = Partition(arr, low, high);
+            QuickSort(arr, low, pivotIndex - 1);
+            QuickSort(arr, pivotIndex + 1, high);
+        }
+    }
+  
+    static void PrintArray(int[] arr)
+    {
+        foreach (int item in arr)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+
+    static void Main()
+    {
+        int[] arr = { 10, 7, 8, 9, 1, 5 };
+        PrintArray(arr);
+        QuickSort(arr, 0, arr.Length - 1);
+        PrintArray(arr);
+    }
+}
